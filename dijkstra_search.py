8fd292db784e4aa6cb83b8d44d488a8d44d7948d
Adeon18 <56467999+Adeon18@users.noreply.github.com>
2021-01-03 13:26:48 +0200

    Finished the documentation on the dijksta search. Added empty main script.

--- dijkstra_search.py
+++ dijkstra_search.py
@@ -1,3 +1,11 @@
+'''
+A module that contains all the functions needed for the dijkstra search
+algoritm.
+
+Functions:
+    find_min_dist: Finds the future stable node, is called in dijkstra_search.
+    dijkstra_search: The whole dijkstra algoritm.
+'''
 import sys
 
 graph = [[0, 4, 0, 0, 0, 0, 0, 8 , 0], 
@@ -14,41 +22,61 @@ vertices = len(graph)
 
 
 def find_min_dist(dist_lst: list, bool_lst: list) -> int: 
-   
-        # Initilaize minimum distance for next node 
-        min_dist = sys.maxsize
+    '''
+    A function that finds a node that can be turned into a stable one but
+    is not yet stable
 
+    Args:
+        dist_lst: The list of all node distances.
+        bool_lst: The list of bools, each one representing if an appropriate
+          node is stable
+    
+    Returns:
+        The index of that node in dist_lst.
+    '''
+    min_dist = sys.maxsize
+
+    for v in range(vertices): 
+        if dist_lst[v] < min_dist and bool_lst[v] == False: 
+            min_dist = dist_lst[v] 
+            min_index = v 
+
+    return min_index
+
+
+
+def dijkstra_search(start: int) -> list: 
+    '''
+    The main dijkstra search algoritm, which works from a certain node.
+
+    Args:
+        start: The index of a start node in dist_list.
+    
+    Returns:
+        Modified dist list with all nodes and their distances from the start
+          one.
+    '''
+    # Create a list of infinites
+    dist_lst = [sys.maxsize] * vertices
+    # Start from a start node
+    dist_lst[start] = 0
+    # No node is yet stable.
+    bool_lst = [False] * vertices
+
+    for _ in range(vertices): 
+
+        # Pick the unstable node with a minimum distance from the start one
+        u = find_min_dist(dist_lst, bool_lst) 
+
+        # That node becomes stable now
+        bool_lst[u] = True
+        # Find if there is a shorter path between nodes
         for v in range(vertices): 
-            if dist_lst[v] < min_dist and bool_lst[v] == False: 
-                min_dist = dist_lst[v] 
-                min_index = v 
-   
-        return min_index
-
-
-
-def dijkstra_search(src): 
-   
-        dist_lst = [sys.maxsize] * vertices
-        dist_lst[src] = 0
-        bool_lst = [False] * vertices
-   
-        for _ in range(vertices): 
-   
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            u = find_min_dist(dist_lst, bool_lst) 
-   
-            # Put the minimum distance vertex in the  
-            # shotest path tree 
-            bool_lst[u] = True
-        
-            for v in range(vertices): 
-                if graph[u][v] > 0 and bool_lst[v] == False\
-                        and dist_lst[v] > (dist_lst[u] + graph[u][v]): 
-                    dist_lst[v] = dist_lst[u] + graph[u][v] 
-        
-        return dist_lst
+            if graph[u][v] > 0 and bool_lst[v] == False\
+                    and dist_lst[v] > (dist_lst[u] + graph[u][v]): 
+                dist_lst[v] = dist_lst[u] + graph[u][v] 
+    
+    return dist_lst
 
 
 
---
+++ main.py
@@ -0,0 +1,4 @@
+'''
+The main sctipt of the dijkstra search of the smallest path between 2 nodes.
+'''
+from dijkstra_search import dijkstra_search, find_min_dist
